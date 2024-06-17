529bc550087b2ab72667741c06cb7a00b30573a2
Rail Ishmuratov <railishm@gmail.com>
2024-06-17 23:04:25 +0300

    New commit

--- main.cpp
+++ main.cpp
@@ -6,13 +6,8 @@
 
 int main()
 {
-    Image im(400, 400);
-
-    im.drawCircle(100, 200, 200, {200, 100, 100});
+    Image im("ppm_examples/zlatoust1910.ppm");
     im.drawCircle(20, 100, 100, {150, 150, 150});
-    im.drawLine(100, 100, 200, 300, {255, 255, 0});
-    im.drawLine(100, 100, 280, 300, {255, 255, 0});
-    im.drawLine(100, 100, 360, 300, {255, 255, 0});
-
+    
     im.savePpm("result.ppm");
 }
